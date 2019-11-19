6f718fbd04195c1cbd47240c78bcc7b03483712b
rodoqa <roarfeqa@outlook.com>
2019-11-19 18:01:10 -0300

    19-nov-2019 log4j2
    -passing log4j 1.2.17 to log4j2 2.12.1

--- src/main/java/listeners/Listener.java
+++ src/main/java/listeners/Listener.java
@@ -1,7 +1,7 @@
 package listeners;
 
-import org.apache.log4j.LogManager;
-import org.apache.log4j.Logger;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
 import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
--- src/test/java/baseTest.java
+++ src/test/java/baseTest.java
@@ -1,7 +1,7 @@
 import io.restassured.RestAssured;
 import io.restassured.authentication.PreemptiveBasicAuthScheme;
-import org.apache.log4j.LogManager;
-import org.apache.log4j.Logger;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 
